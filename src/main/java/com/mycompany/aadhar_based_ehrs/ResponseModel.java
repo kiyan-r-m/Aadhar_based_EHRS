8c12ea6cab604ca70f5c0dc2121087c984eeb619
charmi <ymodi1973@gmail.com>
2023-03-02 12:50:08 +0530

    ResponseModel

---
+++ src/main/java/com/mycompany/aadhar_based_ehrs/ResponseModel.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aadhar_based_ehrs;
+
+/**
+ *
+ * @author admin
+ */
+public class ResponseModel<T> {
+    
+    public boolean status;
+    public String message;
+    public T data;
+
+    public ResponseModel() {
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+    
+    
+}
